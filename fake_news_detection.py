5318d801082bb791ef8c97d9f34e1220e7abec91
Matin Mahmood <mmahmood@exseed.ed.ac.uk>
2022-05-03 09:21:07 +0100

    Add code for detect fake news from real news articles

--- fake_news_detection.py
+++ fake_news_detection.py
@@ -1,31 +1,64 @@
+'''
+The following code creates a Python machine learning project that is able to differentiate between fake news and real news articles.
 
-# Importing Libraries
+Before getting started, please make sure you install the necessary libraries required for this project.
+
+'''
+
+# Import required libraries
 import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.naive_bayes import MultinomialNB
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+from sklearn.metrics import confusion_matrix,accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
-from sklearn.ensemble import RandomForestClassifier
 
-# Importing Dataset
-df = pd.read_csv('dataset.csv')
+# Load the data into a pandas dataframe
+df=pd.read_csv('news.csv')
 
-# Splitting dataset into Train and Test sets
-x_train, x_test, y_train, y_test = train_test_split(df['text'], df['label'], test_size=0.2, random_state=42)
+# Drop any null values
+df=df.dropna()
 
-# Vectorizing Text Data
-tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
-tfidf_train = tfidf_vectorizer.fit_transform(x_train)
-tfidf_test = tfidf_vectorizer.transform(x_test)
+# Split the data into train and test sets
+x_train,x_test,y_train,y_test=train_test_split(df['text'], df['label'], test_size=0.2, random_state=7)
 
-# Building and Training the Model
-model = MultinomialNB()
-model.fit(tfidf_train, y_train)
+# Initialize the TfidfVectorizer
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
 
-# Evaluating the Model
-y_pred = model.predict(tfidf_test)
-score = accuracy_score(y_test, y_pred)
-cm = confusion_matrix(y_test, y_pred)
-print("Accuracy:", score)
-print("Confusion Matrix:", cm)
+# Fit and transform the train set, transform the test set
+tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+# Initialize the PassiveAggressiveClassifier
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+
+# Predict on the test set and calculate accuracy
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+
+# Create confusion matrix to evaluate the model
+confusion_matrix(y_test,y_pred, labels=['FAKE','REAL'])
+
+# Visualize confusion matrix
+cm = confusion_matrix(y_test,y_pred, labels=['FAKE','REAL'])
+sns.heatmap(cm, annot=True, cmap='Blues', fmt='d')
+
+# Test with some own news
+input_text = ['We will have a manned mission to Mars within the next decade.']
+tfidf_input=tfidf_vectorizer.transform(input_text)
+y_pred_input=pac.predict(tfidf_input)
+
+print(y_pred_input)
+
+'''
+The above code can be run from the command line using the following command:
+
+python ml_project.py
+
+Note that the data file 'news.csv' should be present in the same directory as the Python script.
+'''
